dbd7a6a3a97ed74f9da7ccaf74b351a4708ec347
Suneet Jain <suneet.bhopal@gmail.com>
2023-04-17 14:42:45 -0400

    test file for mac

--- Project Handout/test.py
+++ Project Handout/test.py
@@ -1,103 +1,51 @@
-import sys
-import os
+import keras
+from keras.datasets import cifar100
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.callbacks import ModelCheckpoint
 
-import time
+# Load CIFAR-100 dataset
+(x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode='fine')
+y_train = y_train // 5 # Map to 20 superclasses
+y_test = y_test // 5
 
-import numpy as np
-import eagerpy as ep
-import cv2
-import random
+# Define the neural network architecture
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=x_train.shape[1:]))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-import sklearn
-from sklearn.metrics import confusion_matrix
-from art.defences.preprocessor import FeatureSqueezing
-from scipy import ndimage
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-# we'll use tensorflow and keras for neural networks
-import tensorflow as tf
-import tensorflow.keras as keras
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(20, activation='softmax')) # 20 superclasses
 
-import utils # we need this
+# Compile the model with appropriate loss function and optimizer
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-def noisy(noise_typ,image):
-    if noise_typ == "gauss":
-        row,col,ch= image.shape
-        mean = 0
-        var = 0.1
-        sigma = var**0.5
-        gauss = np.random.normal(mean,sigma,(row,col,ch))
-        gauss = gauss.reshape(row,col,ch)
-        noisy = image + gauss
-        return noisy
-    elif noise_typ == "s&p":
-        row,col,ch = image.shape
-        s_vs_p = 0.5
-        amount = 0.004
-        out = np.copy(image)
-        # Salt mode
-        num_salt = np.ceil(amount * image.size * s_vs_p)
-        coords = [np.random.randint(0, i - 1, int(num_salt))
-              for i in image.shape]
-        out[coords] = 1
-        # Pepper mode
-        num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
-        coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in image.shape]
-        out[coords] = 0
-        return out
-    elif noise_typ == "poisson":
-        vals = len(np.unique(image))
-        vals = 2 ** np.ceil(np.log2(vals))
-        noisy = np.random.poisson(image * vals) / float(vals)
-        return noisy
-    elif noise_typ =="speckle":
-        row,col,ch = image.shape
-        gauss = np.random.randn(row,col,ch)
-        gauss = gauss.reshape(row,col,ch)        
-        noisy = image + image * gauss
-        return noisy
+# Define callbacks to save the best model during training
+best_model_file = "best_model.hdf5"
+checkpoint = ModelCheckpoint(best_model_file, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
 
-# Directory
-dir_path = os.path.dirname(os.path.realpath(__file__))
+# Train the model
+batch_size = 128
+epochs = 100
+history = model.fit(x_train, keras.utils.to_categorical(y_train, 20), batch_size=batch_size, epochs=epochs,
+                    validation_data=(x_test, keras.utils.to_categorical(y_test, 20)), shuffle=True,
+                    callbacks=[checkpoint])
 
+# Evaluate the model on the test set
+model.load_weights(best_model_file) # Load the weights of the best model
+scores = model.evaluate(x_test, keras.utils.to_categorical(y_test, 20), verbose=1)
+print("Test accuracy:", scores[1])
 
-
-train_x, train_y, test_x, test_y, val_x, val_y, labels = utils.load_data()
-gauss, snp, poisson, speckle, none = 0,0,0,0,0
-counter = 0
-print(len(val_x))
-for i in val_x:
-    # cv2.imshow()
-    counter += 1
-    x = random.randint(1,8)
-    if x==1:
-        ret = noisy("gauss",i)
-        gauss += 1
-    elif x==2:
-        ret = noisy("s&p",i)
-        snp += 1
-    elif x==3:
-        ret = noisy("poisson",i)
-        poisson += 1
-    elif x==4:
-        ret = noisy("speckle",i)
-        speckle += 1
-    else: 
-        ret = i
-        none += 1
-    if x<=4:
-        subfolder = "generatedImages/noisy"
-    else:
-        subfolder = "generatedImages/nonNoisy"
-    subfolder_path = os.path.join(dir_path, subfolder)
-    if not os.path.exists(subfolder_path):
-        os.makedirs(subfolder_path)
-    img_path = os.path.join(subfolder_path, f"{counter}.jpg")
-    cv2.imwrite(img_path, i*255)
-print("gauss",gauss)
-print("snp",snp)
-print("poisson",poisson)
-print("speckle",speckle)
-print("nonNoisy",none)
 
 
 
